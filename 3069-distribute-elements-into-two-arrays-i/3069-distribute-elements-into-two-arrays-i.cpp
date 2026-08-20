8ace0f944e0d72efa3378e26726950d2c0515fcc
Devesh Sharma <devesh200300@gmail.com>
2026-08-20 23:24:05 +0530

    Time: 4 ms (12.56%), Space: 23.9 MB (66.37%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int>arr1={nums[0]};
+        vector<int>arr2={nums[1]};
+
+        for(int i=2; i<n; i++){
+            if(arr1[arr1.size()-1] > arr2[arr2.size()-1]){
+                arr1.push_back(nums[i]);
+            }else{
+                arr2.push_back(nums[i]);
+            }
+        }
+    
+        for(int num : arr2){
+           arr1.push_back(num);
+        }
+        return arr1;
+    }
+};
