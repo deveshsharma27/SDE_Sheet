111d6ea6ecdc73fe9498c45eec7a4e0d3e411907
Devesh Sharma <devesh200300@gmail.com>
2026-09-18 02:10:37 +0530

    Time: 2 ms (99.39%), Space: 47.4 MB (11.37%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            int needed = target - num;
+
+            if (mp.containsKey(needed)) {
+                return new int[] { mp.get(needed), i };
+            }
+
+            mp.put(nums[i], i);
+        }
+        return new int[] { };
+    }
+}
