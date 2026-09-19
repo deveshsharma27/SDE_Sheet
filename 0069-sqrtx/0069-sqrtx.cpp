5ae0de9be894f353a4eb91499e2724b7586b0684
Devesh Sharma <devesh200300@gmail.com>
2026-09-19 23:37:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.9%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int mySqrt(int x) {
-       long long ans =0;
-
-       for(long long i =1; i<INT_MAX; i++){
-            long long sq = (1LL)*(i*i);
-            if(sq==x){
-                ans = i;
-                break;
-            }else if(sq>x){
-                ans = i-1;
-                break;
-            }
+       if(x<2){
+        return x;
        }
-      return ans; 
+
+    long long left=1;
+    long long  right=x/2;
+
+    long long  ans=1;
+
+      while(left <=right){
+        long long  mid = left+(right-left)/2;
+
+        if(mid*mid <= x){
+            ans = mid;
+            left=mid+1;
+        }else{
+            right = mid-1;
+        }
+      }
+      return ans;
     }
 };
